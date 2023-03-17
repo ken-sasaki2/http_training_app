16a243a1150f58d2ab717fd8927951a2d99f8911
佐々木　謙 <nkekisasa222@gmail.com>
2023-03-17 22:39:01 +0900

    update: APIの結果をModelに格納してFutureBuilderを使ってデータを描画した

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
@@ -49,20 +51,9 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  String? weatherText;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  void get() async {
+  Future<WeatherModel?>get() async {
     const lat = '35.65138';
     const lon = '139.63670';
     const key = 'aa50ce0598c22de20f75cf3ea31ac312';
@@ -73,9 +64,18 @@ class _MyHomePageState extends State<MyHomePage> {
 
     var url = Uri.parse(domain + pass + query);
     debugPrint('url: $url');
+
     var response = await http.get(url);
-    debugPrint('statusCode: ${response.statusCode}');
-    debugPrint('response: ${response.body}');
+
+    if (response.statusCode == 200) {
+      var body = response.body;
+      var decodeData = jsonDecode(body);
+      var json = decodeData['current'];
+      var model = WeatherModel.fromJson(json);
+
+      return model;
+    }
+    return null;
   }
 
   @override
@@ -86,47 +86,58 @@ class _MyHomePageState extends State<MyHomePage> {
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: get,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    //
+
+    return FutureBuilder(
+      future: get(),
+      builder: (BuildContext context, AsyncSnapshot<WeatherModel?> snapshot) {
+        if (snapshot.hasData) {
+          debugPrint('処理完了');
+          debugPrint('data:${snapshot.data}');
+
+          var data = snapshot.data;
+          weatherText = data?.description;
+
+          return Scaffold(
+            body: Center(
+                child: Text('現在の天気は$weatherTextです！')
+            )
+          );
+        } else {
+          debugPrint('処理中...');
+
+          return const Scaffold(
+              body: Center(
+                  child: Text('処理中...')
+              )
+          );
+        }
+      }
     );
+  }
+}
+
+class WeatherModel {
+  final String main;
+  final String description;
+  final String icon;
+
+  WeatherModel({
+    required this.main,
+    required this.description,
+    required this.icon
+  });
+
+  factory WeatherModel.fromJson(Map<String, dynamic> json) {
+    var weather = json['weather'];
+    var data = weather[0];
+
+    var model = WeatherModel(
+        main: data['main'],
+        description: data['description'],
+        icon: data['icon']
+    );
+
+    return model;
   }
 }
